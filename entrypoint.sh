66059c4963116149dabb7823fa365001e77b23e9
g.karateev <g.karateev@g.nsu.ru>
2025-06-07 16:09:08 +0700

    fixed v4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic --noinput
+# python manage.py collectstatic --noinput
 
 exec uvicorn BrainTumor.asgi:application --host 0.0.0.0 --port 8000 --reload
